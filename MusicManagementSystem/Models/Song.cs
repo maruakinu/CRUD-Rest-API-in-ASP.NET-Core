f99cc41cc79262ae7ab3b71a1eb83ee1b2bb917b
maruakinu <marloaquino080621@gmail.com>
2024-01-03 10:40:58 +0800

    Adding validation to mdoel class

--- MusicManagementSystem/Models/Song.cs
+++ MusicManagementSystem/Models/Song.cs
@@ -6,8 +6,14 @@ namespace MusicManagementSystem.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is Required")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Language is Required")]
         public string Language { get; set; }
+
+        [Required(ErrorMessage = "Duration is Required")]
         public string Duration { get; set; }
     }
 }
